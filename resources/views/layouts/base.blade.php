40275b59a076b7b447b0705b600fdd805f49e79e
Waldemar Figueroa Medina <waldemar@nobox.com>
2015-01-18 21:29:01 -0500

    Create new base and master layouts
    Base layout is a skeleton that all other layouts should be based off of. Master layout then starts including app stylesheets and scripts, among other things.

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @section('title')
+        <title>App Title</title>
+    @show
+
+    @section('meta')
+        {{-- Page meta tags --}}
+    @show
+
+    @section('stylesheets')
+        {{-- Page stylesheets --}}
+    @show
+
+    @section('scripts-head')
+        {{-- Scripts required afap --}}
+    @show
+</head>
+
+@section('body-open')
+<body>
+@show
+
+    @yield('body-content')
+
+    @section('scripts-body')
+        {{-- Scripts are loaded last --}}
+    @show
+
+</body>
+</html>
