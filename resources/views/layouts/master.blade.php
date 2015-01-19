f5bdca3ad091c08b16e89912428844833f6885ae
Waldemar Figueroa Medina <waldemar@nobox.com>
2015-01-18 21:29:01 -0500

    Create new base and master layouts
    Base layout is a skeleton that all other layouts should be based off of. Master layout then starts including app stylesheets and scripts, among other things.

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.base')
+
+@section('stylesheets')
+    <link rel="stylesheet" href="{{ elixir('css/styles.css') }}">
+@stop
+
+@section('scripts-head')
+    {{-- Modernizr --}}
+    @if ('production' == $environment)
+    <script src="{{ asset('js/vendor/modernizr.js') }}"></script>
+    @else
+    <script src="{{ asset('bower_components/modernizr/modernizr.js') }}"></script>
+    @endif
+@stop
+
+@section('body-content')
+    @if(file_exists(public_path() . '/svg/symbols.svg'))
+        <?php include(public_path() . '/svg/symbols.svg'); ?>
+    @endif
+
+    @yield('content')
+@stop
+
+@section('scripts-body')
+    <!-- jQuery -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="bower_components/jquery/dist/jquery.min.js"><\/script>')</script>
+
+    <!-- Google Analytics -->
+    <script>
+    @if (empty(getenv('GA_TRACKING_ID')) || getenv('GA_TRACKING_ID') == 'UA-XXXXX-X')
+        console.warn('Remember to set a valid Google Analytics property tracking ID for this app.');
+    @else
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='//www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create', '{{ getenv('GA_TRACKING_ID') }}', 'auto');ga('send','pageview');
+    @endif
+    </script>
+
+    <script src="{{ elixir('js/app.js') }}"></script>
+@stop
