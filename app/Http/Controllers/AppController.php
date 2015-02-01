7d84e603a4600ae5f1dc5f6a48fd873e84e21db0
Waldemar Figueroa Medina <waldemar@nobox.com>
2015-01-31 21:13:07 -0500

    Rearrange starter views, routes and controllers
    - Delete Laravel Home and Welcome controllers. Add our starter App controller.
    - Delete unused views and rework L5 welcome view into the main app layout.

---
+++ app/Http/Controllers/AppController.php
@@ -0,0 +1,14 @@
+<?php namespace App\Http\Controllers;
+
+class AppController extends Controller
+{
+    /**
+     * Show the application index page.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return view('index');
+    }
+}
