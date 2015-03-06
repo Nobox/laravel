e5e4a4737a6006fff4bb1f143c551c80cc82b7a9
Waldemar Figueroa Medina <waldemar@nobox.com>
2015-03-05 20:25:13 -0500

    Include Laravel Debugbar package
    Adds a debug bar with detailed info about app runtime.

--- config/app.php
+++ config/app.php
@@ -145,6 +145,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Packages
+         */
+        Barryvdh\Debugbar\ServiceProvider::class,
+
     ],
 
     /*
@@ -192,6 +197,11 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        /*
+         * Packages
+         */
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
