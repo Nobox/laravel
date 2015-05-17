47081847af28e3bad0f375f686176b9b68f0b92c
Waldemar Figueroa Medina <waldemar@nobox.com>
2015-05-17 11:32:35 -0400

    Add breakpoints module for responsive JS.

---
+++ resources/assets/js/lib/breakpoints.js
@@ -0,0 +1,13 @@
+/**
+ * Media Queries. Should be the same as in CSS.
+ */
+
+var breakpoints = {
+    palm: 'screen and (max-width: 719px)',
+    lap: 'screen and (min-width: 720px) and (max-width: 1023px)',
+    lapAndUp: 'screen and (min-width: 720px)',
+    portable: 'screen and (max-width: 1023px)',
+    desk: 'screen and (min-width: 1024px)'
+}
+
+exports default breakpoints;
