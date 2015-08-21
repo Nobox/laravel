0db7bae26ab51a2499f802e86234e55a717c71f7
Waldemar Figueroa Medina <waldemar@nobox.com>
2015-08-21 15:28:09 -0400

    Disable search engines from crawling staging sites

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,6 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    {{-- Make sure our staging/dev sites aren't crawled by search engines --}}
+    @if ('production' != app()->environment())
+        <meta name="robots" content="noindex">
+    @endif
+
     @section('title')
         <title>App Title</title>
     @show
