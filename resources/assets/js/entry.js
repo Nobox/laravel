73ac11881d03b85115e613b769dce865b04982cd
Waldemar Figueroa Medina <waldemar@nobox.com>
2015-08-24 23:59:44 -0400

    Use svg4everybody from npm directly.
    File is so small it makes no sense to make it a separate request. There is no Modernizr test for this anyway. They use browser-sniffing.

--- resources/assets/js/entry.js
+++ resources/assets/js/entry.js
@@ -1,3 +1,8 @@
-// Disable touch events 300ms delay
 import attachFastClick from 'fastclick';
+import svg4everybody from 'svg4everybody';
+
+// Disable touch events 300ms delay
 attachFastClick(document.body);
+
+// Enable external content for SVGs
+svg4everybody();
