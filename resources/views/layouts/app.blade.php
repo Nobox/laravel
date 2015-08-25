fd6b3ac450b9567880199b4bc0060a67773c6008
Waldemar Figueroa Medina <waldemar@nobox.com>
2015-08-25 00:19:40 -0400

    Tweak GA include script so ga() always exists
    Previously if a tracking ID was not set, the script was not included and ga() would throw undefined errors if events were used.
    
    Now it always includes the script, thus always creating ga(), but the events won't do anything if the tracking ID is invalid. Throws a warning.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,16 +9,18 @@
 
     {{-- Google Analytics --}}
     <script>
-    @if (empty(getenv('GA_TRACKING_ID')) || getenv('GA_TRACKING_ID') == 'UA-XXXXX-X')
-        console.warn('Remember to set a valid Google Analytics property tracking ID for this app.');
-    @else
         (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
         function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
         e=o.createElement(i);r=o.getElementsByTagName(i)[0];
         e.src='//www.google-analytics.com/analytics.js';
         r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-        ga('create', '{{ getenv('GA_TRACKING_ID') }}', 'auto');ga('send','pageview');
-    @endif
+
+        ga('create', '{{ env("GA_TRACKING_ID", "UA-XXXXX-X") }}', 'auto');
+        ga('send', 'pageview');
+
+        @if (empty(env('GA_TRACKING_ID')) || env('GA_TRACKING_ID') == 'UA-XXXXX-X')
+            console.warn('Warning: Remember to set a valid Google Analytics property tracking ID.');
+        @endif
     </script>
 @stop
 
