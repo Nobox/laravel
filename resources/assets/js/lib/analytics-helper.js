98c8ed4b9a45c869e9f4a6c1a711e11b69eb977c
Waldemar Figueroa Medina <waldemar@nobox.com>
2015-08-27 18:24:41 -0400

    Add analytics helper module.
    This file easily lets us track clicks by setting a class and some data attributes with the event information.
    
    Events are delegated to window.document and have a namespaced event.

---
+++ resources/assets/js/lib/analytics-helper.js
@@ -0,0 +1,68 @@
+import $ from 'jquery';
+
+class AnalyticsHelper {
+
+    /**
+     * Initialize helper
+     */
+
+    init () {
+        this.events();
+    }
+
+    /**
+     * Send event to analytics
+     *
+     * @param  {string} category Event category
+     * @param  {string} action   Event action
+     * @param  {string} label    Event label
+     * @param  {string} value    Event value
+     */
+
+    sendEvent(category, action, label, value = null) {
+
+        if (category != null && label != null) {
+            ga('send', 'event', category, action, label, value);
+        } else {
+            console.warn('Category and label are required for GA event bound on: ' + this);
+        }
+
+    }
+
+    /**
+     * Bind analytics helper events. Use event
+     * namespace and delegated to document.
+     */
+
+    events () {
+        var $document = $(document);
+
+        // Generic click helper
+        $document.on('click.analytics', '.js-ga-event', (e) => {
+            var $this = $(this),
+                category = $this.data('ga-category'),
+                action = $this.data('ga-action'),
+                label = $this.data('ga-label'),
+                value = $this.data('ga-value')
+            ;
+
+            this.sendEvent(category, action || 'click', label, value);
+        });
+
+        // Generic hover helper
+        $document.on('mouseover.analytics', '.js-ga-hover-event', (e) => {
+            var $this = $(this),
+                category = $this.data('ga-category'),
+                action = $this.data('ga-action'),
+                label = $this.data('ga-label'),
+                value = $This.data('ga-value')
+            ;
+
+            this.sendEvent('send', 'event', category, action || 'hover', label, value);
+        });
+
+    }
+
+}
+
+export default AnalyticsHelper;
