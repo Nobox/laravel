a93fcc2a52d2fad94ecf2ba890ab3b4e8cd05c9e
Jonathan Torres <jonathan@nobox.com>
2015-12-11 10:36:11 -0500

    Add csrf token meta tag.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf" content="{{ csrf_token() }}">
 
     {{-- Make sure our staging/dev sites aren't crawled by search engines --}}
     @if ('production' != app()->environment())
